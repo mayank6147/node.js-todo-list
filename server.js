e1c713705facbdc94530d1f4a5af04d6b3b9b8ad
mayank-for-learn-99 <1999mmmayank@gmail.com>
2020-08-04 22:58:42 +0530

    3rd commit

--- server.js
+++ server.js
@@ -1,45 +1,64 @@
 // jshint esversion:6
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
 
 const app = express();
-app.set('view engine' ,'ejs');
+var items = [];
+var workList = [];
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-app.get('/',function(req,res){
+/* for serve static file like images , css ,sounds in express we create folder public */
 
-    var today =new  Date();
-    var currentDay = today.getDay();
+app.use(express.static("public"));
 
-    console.log(currentDay);
-    var day = "";
-    switch (currentDay) {
-        case 0:
-            day="sunday";
-            break;
-        case 1:
-            day="monday";
-            break;
-        case 2:
-             day="tuesday";
-            break;
-        case 3:
-            day="wednesday";
-            break;
-        case 4:
-            day="thursday";
-            break;
-        case 5:
-            day="friday";
-            break; 
-        case 6:
-            day="saturday";
-            break;       
-        default:
-            break;
-    }
-    res.render('list',{kindOfDay:day});
+app.get("/", function (req, res) {
+  var today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  /* for sending data to html templete*/
+  res.render("list", {
+    listTitles: day,
+    listItems: items,
+  });
 });
 
-app.listen(3000,function(req,res){
-    console.log('server stared at 3000');
+app.post("/", function (req, res) {
+  console.log(req.body);
+  var item = req.body.newItem;
+  if (item === "" || item === null) {
+ 
+        res.redirect("/");
+  } else {
+    if (req.body.list === "work") {
+      workList.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  }
+});
+app.get("/work", function (req, res) {
+  res.render("list", {
+    listTitles: "work list",
+    listItems: workList,
+  });
+});
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+app.listen(3000, function (req, res) {
+  console.log("server stared at 3000");
 });
